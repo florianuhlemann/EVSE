43ea2ffac7a26dd7159205f161bdd6d84d41bc74
Unknown <florianuhlemann@me.com>
2017-09-01 18:28:49 +0200

    Updates

---
+++ temperature_stm32.c
@@ -0,0 +1,6 @@
+#include "stm32f0xx.h"
+#include "temperature_stm32.h"
+
+void TEMPERATURE_STM32_configure(void) {
+
+}
---
+++ temperature_stm32.h
@@ -0,0 +1,16 @@
+// TEMPERATURE_STM32 library: This library shall enable the internal temperature sensor of an STM32F0 chip.
+
+// Pin Definitions
+
+// USART Definitions
+
+// Variable Definitions
+
+// Parameter Definitions
+
+// Variable Declarations
+
+// Function Definitions
+
+// Function Declarations
+void TEMPERATURE_STM32_configure(void);
