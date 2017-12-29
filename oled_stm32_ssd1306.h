9845f9f265164590117083c60ffff728555238e2
Unknown <florianuhlemann@me.com>
2017-12-29 09:44:21 +0100

    Added drawPixel func to OLED

--- oled_stm32_ssd1306.h
+++ oled_stm32_ssd1306.h
@@ -28,7 +28,7 @@
 
 // Variable Definitions
 #define    OLED_DISPLAY_WIDTH    128
-#define    OLED_DISPLAY_HEIGHT   32
+#define    OLED_DISPLAY_HEIGHT   64
 #define    HIGH                  Bit_SET
 #define    LOW                   Bit_RESET
 #define    COMMAND_BUFFER_LENGTH 26
@@ -39,18 +39,18 @@
 #define    OLED_SETCLOCKDIV      0xD5 // clock divide ratio
 #define    OLED_CLOCKDIVSETTING  0x80 // oscillator frequency
 #define    OLED_SETMULTIPLEX     0xA8 // set MUX ratio
-#define    OLED_MULTIPLEXSETTING 0x1F // MUX ratio is 32-1 = 31 = 0x1F -> height of display
+#define    OLED_MULTIPLEXSETTING 0x3F // MUX ratio depends on height [0x1F = 32px] or [0x3F = 64px]
 #define    OLED_SETDISPLAYOFFSET 0xD3 // Display Offset
 #define    OLED_DISPLAYOFFSET    0x00 // Display Offset = 0
 #define    OLED_SETSTARTLINE     0x40 // Set Start Line
 #define    OLED_CHGPUMPSETTING   0x8D // Set Charge Pump Setting
-#define    OLED_SETCHGPUMP       0x14 // Charge Pump Setting 0x10=Disable 0x14=Enable
+#define    OLED_SETCHGPUMP       0x10 // Charge Pump Setting [0x10 = Disable] or [0x14 = Enable]
 #define    OLED_SETADDRESSMODE   0x20 // Page Addressing Mode
 #define    OLED_HORZPAGEMODE     0x00 // Horizontal with auto new line
 #define    OLED_SEGMENTREMAP     0xA1 // Segment Remap A0 / A1 (Mirroring Horizontally)
 #define    OLED_SCANDIRECTION    0xC8 // Scan Direction C0 / C8 (Mirroring Vertically)
 #define    OLED_SETCOMPINS       0xDA // Set COM pin HW config
-#define    OLED_COMPINSSETTING   0x02 // sequential com pin config (bit 4), disable left/right remap (bit 5)
+#define    OLED_COMPINSSETTING   0x12 // sequential com pin config [0x02 = 32px] or [0x12 = 64px]
 #define    OLED_SETCONTRAST      0x81 // Set Contrast
 #define    OLED_CONTRASTSETTING  0x8F // Contrast Setting
 #define    OLED_SETPRECHGPERIOD  0xD9 // Set Precharge Period
@@ -71,3 +71,5 @@ void OLED_STM32_sendBuffer(uint8_t *buffer, uint8_t bufferType, uint16_t numberO
 void OLED_STM32_digitalWrite(uint16_t GPIO_Pin, BitAction BitVal);
 void OLED_STM32_generateBuffer(uint8_t xOffset, uint8_t yOffset, uint8_t pixelArray[], uint8_t arrayLength);
 void OLED_STM32_updateDisplay(void);
+void OLED_STM32_drawPixel(uint8_t x, uint8_t y);
+void OLED_STM32_drawLine(uint8_t xStart, uint8_t yStart, uint8_t xEnd, uint8_t yEnd);
